9fbeb27158f657a529a0cf3f9acf1fae2d9e8a54
maxkandzia <GIT SERVICE EMAIL>
2021-06-06 14:48:20 +0200

    added az-900 and terraform

--- src/components/BadgesSection.vue
+++ src/components/BadgesSection.vue
@@ -1,16 +1,38 @@
 <template>
   <div class="flex flex-wrap justify-center">
-    <div class="w-6/12 sm:w-4/12 px-4">
+    <div class="w-6/12 sm:w-3/12 px-4">
       <img
         src="@/assets/Bilder/psmi.png"
         alt="..."
+        width="250"
+        height="250"
         class="max-w-full h-auto align-middle border-none"
       />
     </div>
-    <div class="w-6/12 sm:w-4/12 px-4">
+    <div class="w-6/12 sm:w-3/12 px-4">
+      <img
+        src="@/assets/Bilder/terraform.png"
+        alt="..."
+        width="250"
+        height="250"
+        class="max-w-full h-auto align-middle border-none"
+      />
+    </div>
+    <div class="w-6/12 sm:w-3/12 px-4">
+      <img
+        src="@/assets/Bilder/azure-az-900.png"
+        alt="..."
+        width="250"
+        height="250"
+        class="max-w-full h-auto align-middle border-none"
+      />
+    </div>
+    <div class="w-6/12 sm:w-3/12 px-4">
       <img
         src="@/assets/Bilder/aws-saa.png"
         alt="..."
+        width="250"
+        height="250"
         class="max-w-full h-auto align-middle border-none"
       />
     </div>
