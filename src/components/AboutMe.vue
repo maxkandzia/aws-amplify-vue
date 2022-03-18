634a6a751cba25fe8f3cf5507c5d00d2038c13b5
Kandzia, Maximilian <maximilian.kandzia@sva.de>
2022-03-18 17:34:10 +0100

    updatet Tech section

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -92,6 +92,30 @@
             class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
           >
             <dt class="text-sm leading-5 font-medium text-gray-500">
+              Data Warehouse
+            </dt>
+            <dd
+              class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2"
+            >
+              Snowflake
+            </dd>
+          </div>
+          <div
+            class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm leading-5 font-medium text-gray-500">
+              ETL tools
+            </dt>
+            <dd
+              class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2"
+            >
+              Fivetran, dbt
+            </dd>
+          </div>
+          <div
+            class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm leading-5 font-medium text-gray-500">
               Containerization
             </dt>
             <dd
